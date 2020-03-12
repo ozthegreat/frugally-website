2c88bfeba4da817de4dae92fd937069850841027
Edward <e.dakin@fairfx.com>
2020-03-12 11:10:51 +0000

    bucket policy and stuff

--- infrastructure/terraform/cloudfront.tf
+++ infrastructure/terraform/cloudfront.tf
@@ -8,10 +8,11 @@ resource "aws_cloudfront_distribution" "website" {
     ]
   }
 
-  aliases         = [local.endpoint]
-  enabled         = true
-  is_ipv6_enabled = true
-  tags            = local.tags
+  aliases             = [local.endpoint]
+  enabled             = true
+  is_ipv6_enabled     = true
+  tags                = local.tags
+  default_root_object = "index.html"
 
   origin {
     domain_name = aws_s3_bucket.website.website_endpoint
@@ -20,7 +21,7 @@ resource "aws_cloudfront_distribution" "website" {
     custom_origin_config {
       http_port              = 80
       https_port             = 443
-      origin_protocol_policy = "https-only"
+      origin_protocol_policy = "http-only"
       origin_ssl_protocols   = ["TLSv1.2"]
       origin_keepalive_timeout = 5
       origin_read_timeout      = 30
@@ -40,8 +41,8 @@ resource "aws_cloudfront_distribution" "website" {
   }
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
     target_origin_id = aws_s3_bucket.website.id
 
     forwarded_values {
