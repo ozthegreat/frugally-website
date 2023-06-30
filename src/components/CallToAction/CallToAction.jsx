fa88e062ec38908d4ac4b875dc3595e71205070c
Onat <53895969+onattech@users.noreply.github.com>
2023-06-30 17:38:41 +0300

    Mobile responsiveness ready

--- src/components/CallToAction/CallToAction.jsx
+++ src/components/CallToAction/CallToAction.jsx
@@ -1,49 +1,13 @@
 import * as React from 'react';
+import styles from './styles.module.css';
 
 export default function CallToAction() {
   return (
-    <div
-      className="container"
-      style={{
-        margin: 'auto',
-        background: '#000',
-        display: 'flex',
-        padding: '60px',
-        borderRadius: '10px',
-        marginBottom: '100px',
-      }}
-    >
-      <div
-        style={{
-          color: '#fff',
-          bottom: 0,
-          paddingLeft: '2rem',
-          paddingRight: '2rem',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'baseline',
-          flexDirection: 'column',
-        }}
-      >
+    <div className={styles.container}>
+      <div className={styles.content}>
         <h3 style={{ fontSize: '36px' }}>Call to Action</h3>
         <p style={{ maxWidth: 800 }}> Once you've worked through the getting started guide</p>
-        <button
-          style={{
-            background: '#f93d6b',
-            color: '#fff',
-            borderRadius: '70px',
-            justifyContent: 'center',
-            alignItems: 'center',
-            padding: '26px 50px',
-            fontSize: '17px',
-            fontWeight: 500,
-            display: 'flex',
-            border: 'none',
-            cursor: 'pointer',
-          }}
-        >
-          My Action
-        </button>
+        <button className={styles.button}>My Action</button>
       </div>
       <img style={{ borderRadius: '10px' }} src="https://placehold.co/500x350" alt="cta" />
     </div>
